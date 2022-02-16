0f1b9ab19be43bc0a71b6d1cfe7f06df2f954235
gaffney1 <gaffney@oakland.edu>
2022-02-16 13:39:25 -0500

    added in screens
    added in a couple more screens,and the ability to switch between them

--- Attendance/app/src/main/java/com/example/attendance/ClassViewActivity.java
+++ Attendance/app/src/main/java/com/example/attendance/ClassViewActivity.java
@@ -2,13 +2,47 @@ package com.example.attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.attendance.databinding.ActivityLoginBinding;
 
 public class ClassViewActivity extends AppCompatActivity {
+    //private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_view);
+        //binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.my_layout_id);
+
+        final Button button = (Button) findViewById(R.id.HomeButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openHomeActivity();
+            }
+        });
+        final Button ClassesButton = (Button) findViewById(R.id.ClassesButton);
+        ClassesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openClassesActivity();
+            }
+        });
+
+    }
+    private void openClassesActivity()
+    {
+        Intent intent = new Intent(this, ClassViewActivity.class);
+        startActivity(intent);
+
+    }
+    private void openHomeActivity()
+    {
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivity(intent);
+
     }
 }
--- Attendance/app/src/main/java/com/example/attendance/TestActivity.java
+++ Attendance/app/src/main/java/com/example/attendance/TestActivity.java
@@ -2,7 +2,10 @@ package com.example.attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -10,6 +13,31 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        final Button button = (Button) findViewById(R.id.ClassesButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openTestActivity();
+            }
+        });
+        final Button ClassesButton = (Button) findViewById(R.id.HomeButton);
+        ClassesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openHomeActivity();
+            }
+        });
+
+    }
+    private void openTestActivity()
+    {
+        Intent intent = new Intent(this, ClassViewActivity.class);
+        startActivity(intent);
+
+    }
+    private void openHomeActivity()
+    {
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivity(intent);
 
     }
 }
